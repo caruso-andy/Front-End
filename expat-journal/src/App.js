a30bf8ba49b073f9c0386c9fe0fb609a8be2eeed
Jamie Sullivan <jamiesullivan@jamies-mbp.lan>
2020-06-22 10:34:24 -0400

    set up router

--- expat-journal/src/App.js
+++ expat-journal/src/App.js
@@ -1,7 +1,11 @@
 import React, { useState } from 'react';
+import { BrowserRouter as Route, Link, Switch } from 'react-router-dom';
+
 import './App.css';
 
 import Login from './components/Login'
+import Home from './components/Home'
+import PrivateRoute from './utils/PrivateRoute'
 
 const initalCredentials = {
   username: '',
@@ -13,10 +17,24 @@ function App() {
   const [ credentials, setCredentials ] = useState(initalCredentials)
 
 
-
   return (
    <div>
-     <Login credentials={credentials} setCredentials={setCredentials}/>
+     <ul>
+       <li>
+         <Link to='/login'>Login</Link>
+       </li>
+       <li>
+         <Link to='/home'>Home</Link>
+       </li>
+     </ul>
+
+     <Switch>
+          <Route path='/login' component={Login}>
+              <Login credentials={credentials} setCredentials={setCredentials}/>
+          </Route>
+
+          <PrivateRoute path='/home' component={Home}/>
+    </Switch>
    </div>
   )
 }
---
+++ expat-journal/src/components/Card.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+
+const Card = (props) => {
+
+    return (
+        <div>
+            <img>image here</img>
+            <p>description here</p>
+        </div>
+    )
+}
+
+export default Card 
